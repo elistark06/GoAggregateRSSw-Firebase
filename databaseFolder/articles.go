132bb40613ab9ce465b0ab28113b9de41ac3ad6b
Eli Stark <eli_stark@icloud.com>
2025-06-12 21:57:26 -0400

    successfully puled multiple rss feeds into the database

--- databaseFolder/articles.go
+++ databaseFolder/articles.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/mmcdole/gofeed"
-	_ "github.com/mmcdole/gofeed"
 )
 
 // responsible for database interactions
@@ -15,6 +14,7 @@ type ArticleRepository struct {
 
 // simple struct for request methods
 type ArticleRequests struct {
+	Db *sql.DB
 }
 
 // assigns fields for article
@@ -55,19 +55,82 @@ func (r *ArticleRequests) FetchRSSFeed() ([]Article, error) {
 
 	for _, blog := range myBlogs {
 
-		fp := gofeed.NewParser()
-		feed, _ := fp.ParseURL(blog)
+		fp := gofeed.NewParser()     // creates a parser
+		feed, _ := fp.ParseURL(blog) // fetches the feed from the URL and parses it
 		if feed == nil {
 			fmt.Println("No feed found for URL:", blog)
 			continue
 		}
 
-		if len(feed.Items) > 0 {
-			fmt.Println("first result", feed.Items)
+		if len(feed.Items) > 0 { // checks if the feed has items
+			for _, item := range feed.Items {
+
+				// create an article for each item in the feed
+				//  and pass the data to a function that will
+				//  add it to the database
+
+				Article := Article{
+					Title:         item.Title,
+					Link:          item.Link,
+					Content:       item.Content,
+					PublishedDate: item.Published,
+					ReceivedDate:  item.Updated,
+				}
+
+				fmt.Println("Article found:", Article.Title)
+
+				// Insert the article into the database
+				err := r.InsertArticle(Article)
+				if err != nil {
+					fmt.Println("Error inserting article:", err)
+					continue
+				} else {
+					fmt.Println("Article inserted successfully:", Article.Title)
+				}
+
+			}
 		}
+
 	}
 
 	return nil, nil
 }
 
-func (r *ArticleRepository) InsertArticle(article Article) error {
+func (r *ArticleRequests) InsertArticle(article Article) error {
+	var count int
+	// Check if the article already exists in the database
+	err := r.Db.QueryRow(`
+		SELECT COUNT(*) FROM articles
+        WHERE title = ? AND link = ?`,
+		article.Title, article.Link).Scan(&count)
+
+	if err != nil {
+		return fmt.Errorf("error checking if article exists: %v", err)
+	}
+
+	if count > 0 {
+		fmt.Printf("Skipping duplicate article: %s\n", article.Title)
+		return nil
+	}
+
+	// Insert the article into the database
+	result, err := r.Db.Exec(`
+        INSERT INTO articles (title, link, content, publisheddate, receiveddate)
+        VALUES (?, ?, ?, ?, ?)
+    `, article.Title, article.Link, article.Content, article.PublishedDate, article.ReceivedDate)
+	if err != nil {
+		return fmt.Errorf("insert failed: %v", err)
+	}
+
+	// Get the ID of the inserted row
+	id, err := result.LastInsertId()
+	if err != nil {
+		return fmt.Errorf("error getting last insert id: %v", err)
+	}
+
+	// Update the article's ID
+	article.ID = id
+	fmt.Printf("Article inserted with ID: %d - Title: %s\n", id, article.Title)
+
+	return nil
+}
