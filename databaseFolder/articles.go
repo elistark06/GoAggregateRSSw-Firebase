4bda16941fc4325b95000835aaece0de0c662c39
Eli Stark <eli_stark@icloud.com>
2025-06-12 20:37:43 -0400

    able to request recent items from most rss feeds, with error handling

--- databaseFolder/articles.go
+++ databaseFolder/articles.go
@@ -2,12 +2,22 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+
+	"github.com/mmcdole/gofeed"
+	_ "github.com/mmcdole/gofeed"
 )
 
+// responsible for database interactions
 type ArticleRepository struct {
 	Db *sql.DB
 }
 
+// simple struct for request methods
+type ArticleRequests struct {
+}
+
+// assigns fields for article
 type Article struct {
 	ID            int64  `json:"id"`
 	Title         string `json:"title"`
@@ -29,3 +39,35 @@ func (r *ArticleRepository) CreateTable() error {
 
 	return err
 }
+
+func (r *ArticleRequests) FetchRSSFeed() ([]Article, error) {
+	myBlogs := []string{
+		"https://us2.campaign-archive.com/feed?u=5005148108dfbac726f74e31e&id=239e48d26e",
+		"https://www.youtube.com/feeds/videos.xml?channel_id=UCH7xyou6RXO8PKwMZ4nQ64Q",
+		"https://cloudblog.withgoogle.com/products/networking/rss/",
+		"https://feeds.feedburner.com/WebDeveloperJuice",
+		"https://techcrunch.com/tag/saas/feed/",
+		"https://news.google.com/rss/search?hl=en-US&gl=US&q=startups&um=1&ie=UTF-8&ceid=US:en",
+		"https://www.cloudcomputing-news.net/feed/",
+		"https://feeds.feedburner.com/cioreview/fvHK",
+		"https://www.cloudally.com/feed/",
+	}
+
+	for _, blog := range myBlogs {
+
+		fp := gofeed.NewParser()
+		feed, _ := fp.ParseURL(blog)
+		if feed == nil {
+			fmt.Println("No feed found for URL:", blog)
+			continue
+		}
+
+		if len(feed.Items) > 0 {
+			fmt.Println("first result", feed.Items)
+		}
+	}
+
+	return nil, nil
+}
+
+func (r *ArticleRepository) InsertArticle(article Article) error {
--- main.go
+++ main.go
@@ -1,25 +1,51 @@
 package main
 
+// Add these build tags at the top of the file
+
 import (
+	ArticleRequests "GoAggregateRSS/databaseFolder"
 	"database/sql"
-	"log"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3" // Import the SQLite driver
+	_ "github.com/mmcdole/gofeed"   // Import the gofeed package for RSS parsing
 )
 
 func main() {
+
+	ars := &ArticleRequests.ArticleRequests{}
+
+	articles, err := ars.FetchRSSFeed()
+	if err != nil {
+		fmt.Println("Error fetching RSS feed:", err)
+		return
+	}
+
+	if articles == nil {
+		fmt.Println("No articles found.")
+		return
+	} else {
+		fmt.Println("Articles fetched successfully:", len(articles))
+	}
+
 	databaseConnection, err := sql.Open("sqlite3", "./articles.db")
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Deprecated code for creating the table
+	// Uncomment the following lines if you want to create the table in the database
+	/*
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	defer databaseConnection.Close()
+		defer databaseConnection.Close()
 
-	articleRepository := &database.ArticleRepository{Db: databaseConnection}
+		articleRepository := &database.ArticleRepository{Db: databaseConnection}
 
-	err = articleRepository.CreateTable()
-	if err != nil {
-		log.Fatal(err, "ERROR WITH TABLE")
-	}
+
+			err = articleRepository.CreateTable()
+			if err != nil {
+				log.Fatal(err, "ERROR WITH TABLE")
+			}
+	*/
+
 }
